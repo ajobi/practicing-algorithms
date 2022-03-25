4f81d66eeb0030c21496b875df2d11f2da6dc713
Andrej Bilec <andrej.bilec@gmail.com>
2022-03-25 15:23:13 +0100

    :ok_hand: Improve: Shorter solution for BinaryGap

--- codility/lessons/01-iterations/binary-gap.js
+++ codility/lessons/01-iterations/binary-gap.js
@@ -1,37 +1,26 @@
 // SCORE: 100% correctness, performance not assessed for this lesson
 function solution(N) {
-  const binary = toBinary(N)
+  const binary = N.toString(2)
 
-  let longestGap = 0
+  let result = 0
   let currentGap = 0
 
   for (let i = 1; i < binary.length; i++) {
-    if (binary[i] === '0' && binary[i -1] === '1') {
-      currentGap++
-      continue;
+    if (binary[i] === '0' && binary[i - 1] === '1') {
+      currentGap ++
+      continue
     }
 
-    if (currentGap && binary[i] === '0' && binary[i -1] === '0') {
-      currentGap++
-      continue;
+    if (binary[i] === '0' && binary[i - 1] === '0') {
+      currentGap ++
+      continue
     }
 
-    if (currentGap && binary[i] === '1') {
-      longestGap = currentGap > longestGap ? currentGap : longestGap
-      currentGap = 0;
+    if (binary[i] === '1' && binary[i - 1] === '0') {
+      result = Math.max(result, currentGap)
+      currentGap = 0
     }
   }
 
-  return longestGap
-}
-
-function toBinary(dec){
-  let binaryArray = []
-
-  while (dec > 0) {
-    dec % 2 === 1 ? binaryArray.push('1') : binaryArray.push('0')
-    dec = Math.floor(dec / 2)
-  }
-
-  return binaryArray.reverse().join('')
+  return result
 }
