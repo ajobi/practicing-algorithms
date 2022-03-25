c7239a831c7aa3a13a73969e7664cf7704a84fd7
Andrej Bilec <andrej.bilec@gmail.com>
2022-03-25 16:54:41 +0100

    :ok_hand: Improve: Cleaner solution for PermCheck

--- codility/lessons/04-counting-elements/perm-check.js
+++ codility/lessons/04-counting-elements/perm-check.js
@@ -3,12 +3,12 @@ function solution(A) {
   const counter = []
 
   for (let i = 0; i < A.length; i++) {
-    counter[A[i] - 1] = counter[A[i] - 1] ? counter[A[i] - 1] + 1 : 1
+    counter[A[i]] = counter[A[i]] === undefined ? 1 : counter[A[i]] + 1
   }
 
-  for (let count of counter) {
-    if (count !== 1) {
-      return 0;
+  for (let i = 1; i <= A.length; i++) {
+    if (counter[i] !== 1) {
+      return 0
     }
   }
 
