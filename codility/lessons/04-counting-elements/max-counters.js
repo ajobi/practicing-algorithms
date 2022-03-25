eee43566f6fa45fdb35773363cf066bdd4ed58b8
Andrej Bilec <andrej.bilec@gmail.com>
2022-03-25 18:50:48 +0100

    :ok_hand: Improve: Cleaner solution for MaxCounters

--- codility/lessons/04-counting-elements/max-counters.js
+++ codility/lessons/04-counting-elements/max-counters.js
@@ -2,18 +2,19 @@
 function solution(N, A) {
   const counters = new Array(N).fill(0)
 
-  let maxCounter = 0;
-
+  let maxCounter = 0
   for (let i = 0; i < A.length; i++) {
-    if (A[i] === N + 1 && A[i - 1] !== N + 1) {
-      counters.fill(maxCounter)
-      continue
+    if (A[i] === (N + 1) && A[i - 1] === (N + 1)) {
+      continue;
     }
 
-    if (A[i] !== N + 1) {
-      counters[A[i] - 1]++
-      maxCounter = counters[A[i] - 1] > maxCounter ? counters[A[i] - 1]: maxCounter
+    if (A[i] === (N + 1)) {
+      counters.fill(maxCounter)
+      continue;
     }
+
+    counters[A[i] - 1] = counters[A[i] - 1] + 1
+    maxCounter = Math.max(maxCounter, counters[A[i] - 1])
   }
 
   return counters
