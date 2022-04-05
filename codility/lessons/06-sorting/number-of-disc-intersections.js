00e7e0badc6f00dcad53b8854c9adcf657892360
Andrej Bilec <andrej.bilec@gmail.com>
2022-04-05 18:54:59 +0200

    :ok_hand: Improve: Cleaner solution for NumberOfDiscIntersections

--- codility/lessons/06-sorting/number-of-disc-intersections.js
+++ codility/lessons/06-sorting/number-of-disc-intersections.js
@@ -1,20 +1,21 @@
 // SCORE: 100% correctness, 100% performance
 function solution(A) {
-  const sorted = A.map((radius, index) => ({
-    left: index - radius,
-    right: index + radius
+  const sortedDiscs = A.map((radius, x) => ({
+    left: x - radius,
+    right: x + radius
   })).sort((a, b) => a.left - b.left)
 
   let result = 0
+  for (let i = 0; i < sortedDiscs.length - 1; i++) {
+    for (let j = i + 1; j < sortedDiscs.length; j++) {
+      if (sortedDiscs[j].left <= sortedDiscs[i].right) {
+        result++
 
-  for (let i = 0; i < sorted.length; i++) {
-    let j = i + 1
-    while (sorted[j] && sorted[j].left <= sorted[i].right) {
-      result++
-      j++
-
-      if (result > 10000000) {
-        return -1
+        if (result > 10000000) {
+          return -1
+        }
+      } else {
+        break
       }
     }
   }
