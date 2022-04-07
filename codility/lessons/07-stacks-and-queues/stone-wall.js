6926b767a1b9caf0a2b4ae95e9942ffd931970f6
Andrej Bilec <andrej.bilec@gmail.com>
2022-04-07 17:52:53 +0200

    :ok_hand: Improve: Cleaner solution for StoneWall

--- codility/lessons/07-stacks-and-queues/stone-wall.js
+++ codility/lessons/07-stacks-and-queues/stone-wall.js
@@ -1,34 +1,19 @@
 // SCORE: 100% correctness, 100% performance
 function solution(H) {
-  const stack = []
-  let stackSum = 0
   let blockCount = 0
+  let height = 0
+  const stack = []
 
-  for (let height of H) {
-    if (height === stackSum) {
-      continue;
+  for (let i = 0; i < H.length; i++) {
+    while(height > H[i]) {
+      height = height - stack[stack.length - 1]
+      stack.pop()
     }
 
-    if (height > stackSum) {
-      stack.push(height - stackSum)
-      stackSum += height - stackSum
+    if (height < H[i]) {
+      stack.push(H[i] - height)
+      height = height + stack[stack.length - 1]
       blockCount++
-    }
-
-    if (height < stackSum) {
-      while (stackSum > height) {
-        stackSum -= stack.pop()
-      }
-
-      if (height === stackSum) {
-        continue;
-      }
-
-      if (height > stackSum) {
-        stack.push(height - stackSum)
-        stackSum += height - stackSum
-        blockCount++
-      }
     }
   }
 
