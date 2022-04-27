4dc18cbe9bb82531078b0c0dd683e0379542aeb3
Andrej Bilec <andrej.bilec@gmail.com>
2022-04-27 21:53:12 +0200

    :package: New: Easy LeetCode directory added

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('Hello world')
+var maxSubArray = function(nums) {
+  if (nums.length === 1) {
+    return nums[0]
+  }
+
+  const center = Math.floor(nums.length / 2)
+  const left = nums.slice(0, center)
+  const right = nums.slice(center)
+
+  let toLeftMax = Number.MIN_SAFE_INTEGER
+  let currentSum = 0
+
+  for (let i = left.length - 1; i >= 0; i--) {
+    currentSum += left[i]
+    toLeftMax = Math.max(toLeftMax, currentSum)
+  }
+
+  let toRightMax = Number.MIN_SAFE_INTEGER
+  currentSum = 0
+
+  for (let i = 0; i < right.length; i++) {
+    currentSum += right[i]
+    toRightMax = Math.max(toRightMax, currentSum)
+  }
+
+  return Math.max(maxSubArray(left), maxSubArray(right), toRightMax, toLeftMax, toRightMax + toLeftMax)
+};
+
+console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
