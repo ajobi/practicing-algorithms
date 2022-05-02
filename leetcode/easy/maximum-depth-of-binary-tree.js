d748bebefb4849f06fa868d95e9a373d6dcbbdcf
Andrej Bilec <andrej.bilec@gmail.com>
2022-05-02 17:46:22 +0200

    :package: New: MaximumDepthOfBinaryTree solution added

---
+++ leetcode/easy/maximum-depth-of-binary-tree.js
@@ -0,0 +1,11 @@
+var maxDepth = function(root) {
+  if (!root) {
+    return 0
+  }
+
+  if (!root.left && !root.right) {
+    return 1
+  }
+
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
+};
